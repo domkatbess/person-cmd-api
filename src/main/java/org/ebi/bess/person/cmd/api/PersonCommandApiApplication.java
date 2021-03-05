7879d39eb5e4445629cf6b636bcd76df2b681b61
Domkat Bess <domkatbess@gmail.com>
2021-03-05 07:55:05 +0100

    Setup project structure

---
+++ src/main/java/org/ebi/bess/person/cmd/api/PersonCommandApiApplication.java
@@ -0,0 +1,12 @@
+package org.ebi.bess.person.cmd.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonCommandApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonCommandApiApplication.class, args);
+	}
+}
